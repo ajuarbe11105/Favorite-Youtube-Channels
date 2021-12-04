40fe8fd912f43fc2365271afc40ad49a06d2d2e1
alberto Juarbe <albertojuarbe2@gmail.com>
2021-12-04 01:11:06 -0500

    Initial Commit

---
+++ Favorite YT Channels/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorite YT Channels
+//
+//  Created by Alberto Juarbe on 12/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favorite YT Channels/Favorite_YT_ChannelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorite_YT_ChannelsApp.swift
+//  Favorite YT Channels
+//
+//  Created by Alberto Juarbe on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Favorite_YT_ChannelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
